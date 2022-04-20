a4e92e1bddbad6350f0736b4be2914d822a4d7c6
Venkatesh Pothagani <venkateshpothagani@gmail.com>
2022-04-20 07:17:01 +0530

    Minor Changes

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,15 +1,8 @@
-import styles from './AboutPage.module.css';
-
 const AboutPage = () => {
 	return (
 		<div className="h-100 m-auto">
 			<h3>Designed and developed by</h3>
-			<ol>
-				<li>Nirosha Pothagani</li>
-				<li>Sri Harshini</li>
-				<li>Sravani</li>
-			</ol>
-			Under guidance of <span className={styles.high}>Prof. Vijay Kumar</span>
+			<b>Venkatesh Pothagani</b>
 		</div>
 	);
 };
